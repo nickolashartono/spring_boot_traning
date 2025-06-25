14ab465b4065907cef3b8ec58faa51346b3d15c2
nickolashartono <nickz.vict@gmail.com>
2025-06-25 14:49:32 +0700

    Init Day 1 GW Project

---
+++ testday1gw/src/main/java/com/example/testday1gw/Testday1gwApplication.java
@@ -0,0 +1,13 @@
+package com.example.testday1gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testday1gwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testday1gwApplication.class, args);
+	}
+
+}
---
+++ testday1gw/src/test/java/com/example/testday1gw/Testday1gwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testday1gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testday1gwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
